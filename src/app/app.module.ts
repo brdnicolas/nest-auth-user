9d18e121e990e6f31e74b92ca2c6fa9683e7ae61
Nicolas Brouard <brdnicolas.contact@gmail.com>
2024-01-12 14:33:02 +0100

    add app to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { AuthModule } from './auth/auth.module'
+import { AuthModule } from '../auth/auth.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { UserModule } from './user/user.module'
-import { User } from './user/entities/user.entity'
+import { UserModule } from '../user/user.module'
+import { User } from '../user/entities/user.entity'
 
 @Module({
   imports: [
